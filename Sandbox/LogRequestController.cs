c37a5d70e3a40b3a071c9dc12b2d1fc09ce919ab
Jason Estey <esteyj@yahoo.com>
2018-09-05 17:17:04 -0700

    structure the log using json

--- Sandbox/LogRequestController.cs
+++ Sandbox/LogRequestController.cs
@@ -1,20 +1,41 @@
 ﻿using System;
 using System.IO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Sandbox
 {
     public class LogRequestController : Controller
     {
+        private class RequestLogEntry
+        {
+            public string Timestamp { get; private set; }
+            public string UserAgent { get; private set; }
+            public string Host { get; private set; }
+
+            public RequestLogEntry(IHeaderDictionary headers)
+            {
+                Timestamp = $"{DateTime.Now.ToUniversalTime()}";
+                UserAgent = headers["User-Agent"];
+                Host = headers["Host"];
+            }
+        }
+
+        private string GenerateLogEntryAsJson(IHeaderDictionary headers)
+        {
+            return JsonConvert.SerializeObject(new RequestLogEntry(headers));
+        }
+
         [HttpGet]
         [Route("log")]
         public IActionResult Log()
         {
+            string logEntry = GenerateLogEntryAsJson(Request.Headers);
+
             using (StreamWriter log = new StreamWriter("log.txt", true))
             {
-                log.WriteLine($"{DateTime.Now}");
-                log.WriteLine($"User-Agent: {Request.Headers["User-Agent"]}");
-                log.WriteLine($"Host: {Request.Headers["Host"]}");
+                log.WriteLine(logEntry);
             }
 
             return Ok();
